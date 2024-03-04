469cabfc087148a8cdc1b2b7fe68bed41f0818a1
washwin <210010060@iitdh.ac.in>
2024-03-04 16:15:37 +0530

    for server

--- webscraping/paochelsalaitaret.py
+++ webscraping/paochelsalaitaret.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
 
 def download_pdf(url, name, directory):
     save_path = f"downloaded_pdfs/{directory}/{name}"
     response = requests.get(url)
     if response.status_code == 200:
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)  # Create directory if not exist
         with open(save_path, 'wb') as pdf_file:
             pdf_file.write(response.content)
         print(f"PDF downloaded successfully and saved at: {save_path}")
@@ -20,9 +22,9 @@ def fetch_paochelsalaitaret():
 
     for link in http_links:
         url = f"http://paochelsalaitaret.net/{link.get('href')}"
-        name = link.get('href').split('/')
+        name = re.search(r'[^/]+\.pdf$', url).group(0)  # Extract filename from URL using regex
         print(url)
         print(name)
-        download_pdf(url, name[1], directory)
+        download_pdf(url, name, directory)
 
 fetch_paochelsalaitaret()
