7e73802e2adb38d965451bae8e905607b1ab3cae
washwin <210010060@iitdh.ac.in>
2024-03-15 00:20:48 +0530

    new csv

--- twitter-scraper/manipur_violence_tweets.py
+++ twitter-scraper/manipur_violence_tweets.py
@@ -21,8 +21,8 @@ NEXTPAGE = 1
 
 with open('manipur_violence.csv', 'w', newline='') as output_csv_file:
     csv_writer = csv.writer(output_csv_file)
-    csv_writer.writerow(['hashtag', 'text', 'favorite_count'])    
-    
+    # csv_writer.writerow(['hashtag', 'text', 'favorite_count'])    
+    csv_writer.writerow(['id', 'text', 'favorite_count', 'created_at', 'author', 'language', 'retweet_count', 'media', 'view_count', 'replies'])
     for hashtag in hashtags:
         print(f"Finding tweets on {hashtag}")
         while True:
@@ -30,18 +30,20 @@ with open('manipur_violence.csv', 'w', newline='') as output_csv_file:
                 tweets = client.search_tweet(hashtag, 'Latest')
                 for tweet in tweets:
                     tweet = client.get_tweet_by_id(tweet.id)
-                    csv_writer.writerow([hashtag, tweet.text, tweet.favorite_count])
+                    # csv_writer.writerow([hashtag, tweet.text, tweet.favorite_count])
+                    csv_writer.writerow([tweet.id, tweet.text, tweet.favorite_count, tweet.created_at, tweet.user.name, tweet.lang, tweet.retweet_count, tweet.media, tweet.view_count, tweet.replies])
                     
                 # for i in range(NEXTPAGE):
                 #     more_tweets = client.search_tweet(hashtag, 'Latest')
                 #     for tweet in more_tweets:
                 #         tweet = client.get_tweet_by_id(tweet.id)
                 #         csv_writer.writerow([hashtag, tweet.text, tweet.favorite_count])
+                        # csv_writer.writerow([tweet.id, tweet.text, tweet.favorite_count, tweet.created_at, tweet.user.name, tweet.lang, tweet.retweet_count, tweet.media, tweet.view_count, tweet.replies])
                 
                 break
             except twikit.errors.TooManyRequests:
                 print("Rate limit exceeded. Waiting for 15 minutes...")
-                time.sleep(15 * 60)
+                time.sleep(1000)
             except Exception as e:
                 print("An error occurred:", e)
                 continue
